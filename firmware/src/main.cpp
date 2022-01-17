d490e72289f303c5bdb18645b207b5201f396c24
Яков Васильев <yakov.vasilev.95@mail.ru>
2022-01-17 19:52:24 +0900

    encoder readings added

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+//motor PINS
 #define MOTOR_1_A 2
 #define MOTOR_1_B 3
 #define MOTOR_2_A 4
@@ -7,6 +8,8 @@
 #define MOTOR_3_B 9
 #define MOTOR_4_A 10
 #define MOTOR_4_B 11
+
+//encoder PINS
 #define ENCODER_1_A 12
 #define ENCODER_1_B 13
 #define ENCODER_2_A 30
@@ -16,24 +19,81 @@
 #define ENCODER_4_A 48
 #define ENCODER_4_B 49
 
-long enc_tick_1 = 0;
-long enc_tick_2 = 0;
-long enc_tick_3 = 0;
-long enc_tick_4 = 0;
+//encoder ticks counter
+volatile long enc_tick_1 = 0;
+volatile long enc_tick_2 = 0;
+volatile long enc_tick_3 = 0;
+volatile long enc_tick_4 = 0;
 
-bool sig_enc_1_A = false;
-bool sig_enc_1_B = false;
-bool sig_enc_2_A = false;
-bool sig_enc_2_B = false;
-bool sig_enc_3_A = false;
-bool sig_enc_3_B = false;
-bool sig_enc_4_A = false;
-bool sig_enc_4_B = false;
+//encoder signals state
+volatile bool sig_enc_1_A = false;
+volatile bool sig_enc_1_B = false;
+volatile bool sig_enc_2_A = false;
+volatile bool sig_enc_2_B = false;
+volatile bool sig_enc_3_A = false;
+volatile bool sig_enc_3_B = false;
+volatile bool sig_enc_4_A = false;
+volatile bool sig_enc_4_B = false;
+
+enum MOTORS {MOTOR_1, MOTOR_2, MOTOR_3, MOTOR_4};
+
+//Interrupt Service Routine for encoder #1
+void encoderReadA1() {
+  sig_enc_1_A = digitalRead(ENCODER_1_A);
+  if(sig_enc_1_B != sig_enc_1_A) 
+    enc_tick_1++;
+  else enc_tick_1--;
+}
+
+void encoderReadB1() {
+  sig_enc_1_B = digitalRead(ENCODER_1_B);
+  if(sig_enc_1_B == sig_enc_1_A) enc_tick_1++;
+  else enc_tick_1--;
+}
+
+//Interrupt Service Routine for encoder #2
+void encoderReadA2() {
+  sig_enc_2_A = digitalRead(ENCODER_2_A);
+  if(sig_enc_2_B != sig_enc_2_A) enc_tick_2++;
+  else enc_tick_2--;
+}
+
+void encoderReadB2() {
+  sig_enc_2_B = digitalRead(ENCODER_2_B);
+  if(sig_enc_2_B == sig_enc_2_A) enc_tick_2++;
+  else enc_tick_2--;
+}
+
+//Interrupt Service Routine for encoder #3
+void encoderReadA3() {
+  sig_enc_3_A = digitalRead(ENCODER_3_A);
+  if(sig_enc_3_B != sig_enc_3_A) enc_tick_3++;
+  else enc_tick_3--;
+}
+
+void encoderReadB3() {
+  sig_enc_3_B = digitalRead(ENCODER_3_B);
+  if(sig_enc_3_B == sig_enc_3_A) enc_tick_3++;
+  else enc_tick_3--;
+}
+
+//Interrupt Service Routine for encoder #4
+void encoderReadA4() {
+  sig_enc_4_A = digitalRead(ENCODER_4_A);
+  if(sig_enc_4_B != sig_enc_4_A) enc_tick_4++;
+  else enc_tick_4--;
+}
+
+void encoderReadB4() {
+  sig_enc_4_B = digitalRead(ENCODER_4_B);
+  if(sig_enc_4_B == sig_enc_4_A) enc_tick_4++;
+  else enc_tick_4--;
+}
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(115200);
   while (!Serial);
-  // put your setup code here, to run once:
   pinMode(MOTOR_1_A, OUTPUT);
   pinMode(MOTOR_1_B, OUTPUT);
   pinMode(MOTOR_2_A, OUTPUT);
@@ -42,27 +102,103 @@ void setup() {
   pinMode(MOTOR_3_B, OUTPUT);
   pinMode(MOTOR_4_A, OUTPUT);
   pinMode(MOTOR_4_B, OUTPUT);
-  attachInterrupt(ENCODER_1_A, encoder_A_Read(ENCODER_1_A, enc_tick_1, sig_enc_1_A, sig_enc_1_B), CHANGE);
-  attachInterrupt(ENCODER_1_B, encoder_A_Read(ENCODER_1_B, enc_tick_1, sig_enc_1_A, sig_enc_1_B), CHANGE);
+  pinMode(ENCODER_1_A, INPUT);
+  pinMode(ENCODER_1_B, INPUT);
+  pinMode(ENCODER_2_A, INPUT);
+  pinMode(ENCODER_2_B, INPUT);
+  pinMode(ENCODER_3_A, INPUT);
+  pinMode(ENCODER_3_B, INPUT);
+  pinMode(ENCODER_4_A, INPUT);
+  pinMode(ENCODER_4_B, INPUT);
+  sig_enc_1_A = digitalRead(ENCODER_1_A);
+  sig_enc_1_B = digitalRead(ENCODER_1_B);
+  sig_enc_2_A = digitalRead(ENCODER_2_A);
+  sig_enc_2_B = digitalRead(ENCODER_2_B);
+  sig_enc_3_A = digitalRead(ENCODER_3_A);
+  sig_enc_3_B = digitalRead(ENCODER_3_B);
+  sig_enc_4_A = digitalRead(ENCODER_4_A);
+  sig_enc_4_B = digitalRead(ENCODER_4_B);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_1_A), encoderReadA1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_1_B), encoderReadB1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_2_A), encoderReadA2, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_2_B), encoderReadB2, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_3_A), encoderReadA3, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_3_B), encoderReadB3, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_4_A), encoderReadA4, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_4_B), encoderReadB4, CHANGE);
 }
 
-void encoder_A_Read(int16_t enc, long tick, bool sigA, bool sigB) {
-  sigA = digitalRead(enc);
-  if(sigB != sigA) tick++;
-  else tick--;
+void motorGo(MOTORS motor, String direct, uint16_t pwm) {
+  switch (motor)
+  {
+  case MOTOR_1:
+    if (direct == "CW") {
+      analogWrite(MOTOR_1_A, pwm);
+      digitalWrite(MOTOR_1_B, LOW);
+    }
+    else if (direct == "CWW")
+    {
+      analogWrite(MOTOR_1_B, pwm);
+      digitalWrite(MOTOR_1_A, LOW);
+    }
+    else {
+      digitalWrite(MOTOR_1_A, LOW);
+      digitalWrite(MOTOR_1_B, LOW);
+    }
+    break;
+  case MOTOR_2:
+    if (direct == "CW") {
+      analogWrite(MOTOR_2_A, pwm);
+      digitalWrite(MOTOR_2_B, LOW);
+    }
+    else if (direct == "CWW")
+    {
+      analogWrite(MOTOR_2_B, pwm);
+      digitalWrite(MOTOR_2_A, LOW);
+    }
+    else {
+      digitalWrite(MOTOR_2_A, LOW);
+      digitalWrite(MOTOR_2_B, LOW);
+    }
+    break;
+  case MOTOR_3:
+    if (direct == "CW") {
+      analogWrite(MOTOR_3_A, pwm);
+      digitalWrite(MOTOR_3_B, LOW);
+    }
+    else if (direct == "CWW")
+    {
+      analogWrite(MOTOR_3_B, pwm);
+      digitalWrite(MOTOR_3_A, LOW);
+    }
+    else {
+      digitalWrite(MOTOR_3_A, LOW);
+      digitalWrite(MOTOR_3_B, LOW);
+    }
+    break;
+  case MOTOR_4:
+    if (direct == "CW") {
+      analogWrite(MOTOR_4_A, pwm);
+      digitalWrite(MOTOR_4_B, LOW);
+    }
+    else if (direct == "CWW")
+    {
+      analogWrite(MOTOR_4_B, pwm);
+      digitalWrite(MOTOR_4_A, LOW);
+    }
+    else {
+      digitalWrite(MOTOR_4_A, LOW);
+      digitalWrite(MOTOR_4_B, LOW);
+    }
+    break;
+  default:
+    break;
+  } 
 }
 
-
-void encoder_A_Read(int16_t enc, long tick, bool sigA, bool sigB) {
-  sigB = digitalRead(enc);
-  if(sigB == sigA) tick++;
-  else tick--;
-}
-
-void motor_Go(int16_t motor, int16_t direct, uint16_t pwm) {
-
-}
 void loop() {
   // put your main code here, to run repeatedly:
+  motorGo(MOTOR_1, "CW", 200);
+  Serial.println(enc_tick_1);  
 
 }
